41e778df6084d6180248ff11a9430b6e5c9dfb6b
Rouane-12 <djossouvirouane6@gmail.com>
2026-04-01 20:56:14 +0200

    changement de certaines choses2

--- auth.py
+++ auth.py
@@ -25,4 +25,4 @@ def decode_token(token: str):
     return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
 def generate_otp():
-    return "".join(random.choices(string.digits, k=6))
+    return "".join(random.choices(string.digits, k=6)) 
